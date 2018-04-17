f509fb647200551738c50d631971ae75b4475f04
tymiurge <tmrlabas@gmail.com>
2018-04-17 19:19:18 +0300

    DEV: adding api layer

--- src/reducers/cosmos.js
+++ src/reducers/cosmos.js
@@ -1,3 +1,4 @@
+import * as api from './../api'
 
 const COSMOS_CONTENT_REQUEST = 'cosmos/content/request'
 const CARD_ADD_REQUEST = 'card/add/request'
@@ -20,68 +21,12 @@ export default cosmos
 // ***************************************************************
 
 
-const _content = [
-    {
-        id: 1,
-        type: 'box',
-        name: 'refregerator',
-        tags: ['tag1'],
-        progress: 90
-    }, 
-    {
-        id: 2, 
-        type: 'box',
-        name: 'waiting for godoth',
-        tags: ['t1', 't2'],
-        progress: 89
-    },
-    {
-        id: 3,
-        type: 'box',
-        name: 'linux',
-        tags: ['t2', 'linux'],
-        progress: 10
-    },
-    {
-        id: 4,
-        type: 'box',
-        name: 'romeo and juliet',
-        tags: ['t3', 't4'],
-        progress: 48
-    },
-    {
-        id: 5,
-        type: 'box',
-        name: 'leviaphan wakes',
-        tags: ['t5', 't6'],
-        progress: 25
-    }, 
-    {
-        id: 6,
-        type: 'simple',
-        name: 'confined',
-        progress: 6 
-    },
-    {
-        id: 7,
-        type: 'simple',
-        name: 'limp',
-        progress: 0, 
-    },
-    {
-        id: 8,
-        type: 'simple',
-        name: 'crank',
-        progress: 3
-    }
-]
-
-export const fetchContent = parentId => dispatch => {
-    dispatch({
+export const fetchContent = parentId => dispatch => 
+    api.fetchCards(parentId).then(content => dispatch({
         type: COSMOS_CONTENT_REQUEST,
-        content: _content
-    })
-}
+        content
+    })) 
+
 
 // ACTIONS 
 
